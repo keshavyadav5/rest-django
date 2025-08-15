7da62ea4015f5dcf66afa2761faaaa49dc1414b2
keshayadav5 <kesavyadav992@gmail.com>
2025-08-16 03:24:31 +0530

    add employee class and fetch, post, put, delete functionality with class based

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
   path('students/', views.students),
-  path('students/<int:pk>/', views.studentDetails)
+  path('students/<int:pk>/', views.studentDetails),
+
+  #employee urls
+  path('employees/', views.Employees.as_view()),
+  path('employees/<int:pk>/', views.EmployeeDetails.as_view())
 ]
